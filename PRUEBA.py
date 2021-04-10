fa6ce3445009b3e3988d591b7658fce642f6df1a
Rodney <rrojas@paraguayeduca.org>
2021-04-09 20:49:20 -0400

    agregue un led más

--- PRUEBA.py
+++ PRUEBA.py
@@ -2,10 +2,13 @@ from gpiozero import LED
 from time import sleep
 
 led = LED(17)
+led1 = LED(18)
 
 while True:
     led.on()
+    led1.on()
     sleep(1)
     led.off()
+    led1.off()
     sleep(1)
     print("OK")
